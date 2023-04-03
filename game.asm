05f2a9466dcce4dd7f0fdcfa683d039ecf357f3b
Eddy Chen <eddy.al.chen@gmail.com>
2023-04-02 22:28:34 -0400

    now displays arrow on user's button selection

--- game.asm
+++ game.asm
@@ -40,8 +40,13 @@
 .eqv CH_WIDTH 0
 .eqv ENM_HEIGHT 0
 .eqv ENM_WIDTH 0
+.eqv DARK_BLUE 0x3f48cc
+.eqv RED 0xed1c24
+.eqv LIGHT_BLUE 0x7092be
+.eqv BLACK 0x000000
 
 .include "menu.inc"
+.include "menuarrow.inc"
 
 .data
 CH_COLOUR:	.word 0
@@ -65,6 +70,16 @@ START:
 	jal InitializeMenu
 	
 	# Run Menu Loop
+MENULOOP2:
+	
+	# Check Player input
+	
+	# Call MenuArrow if needed
+	
+	# Break loop if needed
+	
+	# j MENULOOP2
+MENULOOPEND:
 	
 	# Initialize Game
 	
@@ -82,19 +97,71 @@ END:
 # Initializes bitmap display to menu
 InitializeMenu:
 	li $t0, BASE_ADDRESS # $t0 stores the base address for display
-	li $t1, 0xffffff # $t1 stores white colour code
-	li $t2, 0x000000 # $t2 stores black colour code
 	move $t4, $t0
 	addi $t5, $t4, 262144
 	la $t6, menu_scr
 MENULOOP1:
-	beq $t4, $t5, MENULOOP2
+	beq $t4, $t5, MENUDONE
 	lw $t7, 0($t6)
 	sw $t7, 0($t4)
 	addi $t4, $t4, 4
 	addi $t6, $t6, 4
 	j MENULOOP1
-MENULOOP2:
+MENUDONE:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	li $t7, 0
+	jal MenuArrow
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
+	
+# Draw menu arrow pointing to a certain button
+MenuArrow:
+	# Draw arrow
+	li $t0, BASE_ADDRESS
+	la $t1, menu_arr
+	li $t2, 0
+	li $t3, 35
+	move $t4, $t1
+	addi $t4, $t4, 4900
+	addi $t0, $t0, 137344
+	beqz $t7, ARROWLOOP1
+	addi $t0, $t0, 50176 # Change location of arrow drawn
+ARROWLOOP1:
+	beq $t1, $t4, ARROWDRAWN
+	lw $t5, 0($t1)
+	sw $t5, 0($t0)
+	addi $t1, $t1, 4
+	addi $t0, $t0, 4
+	addi $t2, $t2, 1
+	bne $t2, $t3, ARROWJUMP1
+	li $t2, 0
+	addi $t0, $t0, 884 # Next line
+ARROWJUMP1:
+	j ARROWLOOP1
+ARROWDRAWN:
+	li $t0, BASE_ADDRESS
+	li $t1, RED
+	li $t2, 0
+	li $t3, 35
+	addi $t0, $t0, 187520
+	move $t4, $t0
+	addi $t4, $t4, 35840
+	beqz $t7, ARROWLOOP2
+	subi $t0, $t0, 50176 # Change location of arrow erased
+	subi $t4, $t4, 50176
+ARROWLOOP2:
+	beq $t0, $t4, ARROWERASED
+	sw $t1, 0($t0)
+	addi $t0, $t0, 4
+	addi $t2, $t2, 1
+	bne $t2, $t3, ARROWJUMP2
+	li $t2, 0
+	addi $t0, $t0, 884
+ARROWJUMP2:
+	j ARROWLOOP2
+ARROWERASED:
 	jr $ra
 
 # Initializes bitmap display for game
