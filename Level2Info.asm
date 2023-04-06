a9885a71cd575c23315039560417283d3e5141ed
Eddy Chen <eddy.al.chen@gmail.com>
2023-04-05 22:12:16 -0400

    moved enemy in level 2

--- Level2Info.asm
+++ Level2Info.asm
@@ -1,5 +1,5 @@
 .data
-Level2PlatNo:	.word 5
-Level2PlatColl:	.word 75, 255, 32, 34, 0, 48, 49, 51, 37, 174, 86, 88, 109, 174, 124, 126, 126, 174, 165, 167, 256, -1, 197, -1
-Level2EnemyNo:	.word 0
-Level2Enemy:	.word 0
+Level2PlatNo:	.word 6
+Level2PlatColl:	.word 75, 255, 32, 34, 0, 48, 49, 51, 37, 174, 86, 88, 109, 174, 124, 126, 126, 174, 165, 167, 0, 35, 96, 98, 256, -1, 197, -1
+Level2EnemyNo:	.word 1
+Level2Enemy:	.word 0, 70, 0
